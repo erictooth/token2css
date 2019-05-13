0911934abbd3791bd18a749e64b08115b33efbdf
atomicpages <atomicpages@gmail.com>
2019-05-13 14:16:53 -0700

    Adding normalize function

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+const normalizePrimitives = value => {
+  if (typeof value === "number") {
+    return value + "";
+  }
+
+  return value;
+};
+
 module.exports = function({ format }) {
   const formatVariable = (key, value) => {
     switch (format) {
@@ -18,21 +26,13 @@ module.exports = function({ format }) {
     let tokens = [];
 
     if (typeof prop === "string" || typeof prop === "number") {
-      if (typeof prop === "number") {
-        prop = prop + "";
-      }
-
-      tokens.push(formatVariable(token, prop));
+      tokens.push(formatVariable(token, normalizePrimitives(prop)));
     } else if (typeof prop === "object") {
       tokens = tokens.concat(
         [].concat(
           ...Object.entries(prop).map(([key, value]) => {
-            if (typeof value === "number") {
-              value = value + "";
-            }
-
             const prevStr = token ? `${token}--` : "";
-            return toString(`${prevStr}${key}`, value);
+            return toString(`${prevStr}${key}`, normalizePrimitives(value));
           })
         )
       );
